c3336d3f6bc924f0c68e066d907c8b125e74d9b2
Angold-4 <wangold4w@gmail.com>
2022-11-18 13:15:10 +0800

    implement the train process

--- lossyrnn/train.py
+++ lossyrnn/train.py
@@ -7,7 +7,6 @@ import torch.optim as optim
 import torch.optim.lr_scheduler as LS
 import torch.utils.data as data
 
-from torch.autograd import Variable
 from torchvision import transforms, datasets
 
 """ Parse the arguments """
@@ -94,4 +93,97 @@ def save(index, epoch=True):
 
 # Decays the learning rate of each parameter group by gamma 
 # once the number of epoch reaches one of the milestones.
+# performs backward on this final model
 scheduler = LS.MultiStepLR(solver, milestones=[3, 10, 20, 50, 100], gamma=0.5)
+
+last_epoch = 0
+if args.checkpoint:
+    resume(args.checkpoint)
+    last_epoch = args.checkpoint
+    scheduler.last_epoch = last_epoch - 1
+
+for epoch in range(last_epoch + 1, args.max_epochs + 1):
+    scheduler.step()
+    for batch, data in enumerate(train_loader):
+        batch_t0 = time.time()
+
+        if args.cuda:
+            ## init lstm state -> The hidden layer (cell state)
+            encoder_h_1 = (torch.zeros(data.size(0), 256, 8, 8).cuda(),
+                           torch.zeros(data.size(0), 256, 8, 8).cuda())
+            encoder_h_2 = (torch.zeros(data.size(0), 512, 4, 4).cuda(),
+                           torch.zeros(data.size(0), 512, 4, 4).cuda())
+            encoder_h_3 = (torch.zeros(data.size(0), 512, 2, 2).cuda(),
+                           torch.zeros(data.size(0), 512, 2, 2).cuda())
+
+            decoder_h_1 = (torch.zeros(data.size(0), 512, 2, 2).cuda(),
+                           torch.zeros(data.size(0), 512, 2, 2).cuda())
+            decoder_h_2 = (torch.zeros(data.size(0), 512, 4, 4).cuda(),
+                           torch.zeros(data.size(0), 512, 4, 4).cuda())
+            decoder_h_3 = (torch.zeros(data.size(0), 256, 8, 8).cuda(),
+                           torch.zeros(data.size(0), 256, 8, 8).cuda())
+            decoder_h_4 = (torch.zeros(data.size(0), 128, 16, 16).cuda(),
+                           torch.zeros(data.size(0), 128, 16, 16).cuda())
+            patches = data.cuda()
+
+        else:
+            encoder_h_1 = (torch.zeros(data.size(0), 256, 8, 8),
+                           torch.zeros(data.size(0), 256, 8, 8))
+            encoder_h_2 = (torch.zeros(data.size(0), 512, 4, 4),
+                           torch.zeros(data.size(0), 512, 4, 4))
+            encoder_h_3 = (torch.zeros(data.size(0), 512, 2, 2),
+                           torch.zeros(data.size(0), 512, 2, 2))
+
+            decoder_h_1 = (torch.zeros(data.size(0), 512, 2, 2),
+                           torch.zeros(data.size(0), 512, 2, 2))
+            decoder_h_2 = (torch.zeros(data.size(0), 512, 4, 4),
+                           torch.zeros(data.size(0), 512, 4, 4))
+            decoder_h_3 = (torch.zeros(data.size(0), 256, 8, 8),
+                           torch.zeros(data.size(0), 256, 8, 8))
+            decoder_h_4 = (torch.zeros(data.size(0), 128, 16, 16),
+                           torch.zeros(data.size(0), 128, 16, 16))
+            patches = data.cpu()
+
+        solver.zero_grad()
+
+        losses = []
+
+        res = patches - 0.5
+
+        bp_t0 = time.time()
+
+
+        """ Perform many iterations in the same data """
+
+        for _ in range(args.iterations):
+            # we call that encoder, means call the forward function (encoder_h_x stands for lstm cell state in our model)
+            # for every data, it is different, init 0 info, for the same data, it is accumulated
+            encoded, encoder_h_1, encoder_h_2, encoder_h_3 = encoder(res, encoder_h_1, encoder_h_2, encoder_h_3)
+            codes = binarizer(encoded)
+            output, decoder_h_1, decoder_h_2, decoder_h_3, decoder_h_4 = decoder(
+                    codes, decoder_h_1, decoder_h_2, decoder_h_3, decoder_h_4)
+
+            res = res - output
+            losses.append(res.abs().mean())
+
+        bp_t1 = time.time()
+        # Sum of the residuals
+        loss = sum(losses) / args.iterations
+        loss.backward() # Tensor.backward(), calculate gradiance
+
+        solver.step()   # solver update the model
+
+        batch_t1 = time.time()
+
+        print('[TRAIN] Epoch[{}]({}/{}); Loss: {:.6f}; Backpropagation: {:.4f} sec; Batch: {:.4f} sec'.
+            format(epoch, batch + 1, len(train_loader), loss.data[0], bp_t1 - bp_t0, batch_t1 - batch_t0)) 
+
+        print(('{:.4f} ' * args.iterations + '\n').format(* [l.data[0] for l in losses]))
+
+        index = (epoch - 1) * len(train_loader) + batch
+
+        ## save checkpoint every 500 training steps
+        if index % 500 == 0:
+            save(0, False)
+
+    save(epoch)
