10cb1f69d02a6b70ebaaa5f4efce5c8f9c758547
Angold-4 <wangold4w@gmail.com>
2023-04-13 07:08:15 +0800

    fix bug

--- learnedc/network.py
+++ learnedc/network.py
@@ -18,11 +18,12 @@ class Encoder(nn.Module):
             nn.Conv2d(1024, 2048, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
         )
-        self.fc_mu = nn.Linear(2048 * 3 * 3, latent_dim)
-        self.fc_logvar = nn.Linear(2048 * 3 * 3, latent_dim)
+        self.fc_mu = nn.Linear(2048 * 1 * 1, latent_dim)
+        self.fc_logvar = nn.Linear(2048 * 1 * 1, latent_dim)
 
     def forward(self, x):
         x = self.conv_layers(x)
+        print("Tensor shape after conv_layers:", x.shape)
         x = x.view(x.size(0), -1)
         mu = self.fc_mu(x)
         logvar = self.fc_logvar(x)
@@ -31,7 +32,7 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, latent_dim):
         super(Decoder, self).__init__()
-        self.fc = nn.Linear(latent_dim, 2048 * 3 * 3)
+        self.fc = nn.Linear(latent_dim, 2048 * 1 * 1)
         self.conv_layers = nn.Sequential(
             nn.ConvTranspose2d(2048, 1024, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
@@ -49,7 +50,7 @@ class Decoder(nn.Module):
 
     def forward(self, x):
         x = self.fc(x)
-        x = x.view(x.size(0), 2048, 3, 3)
+        x = x.view(x.size(0), 2048, 1, 1)
         x = self.conv_layers(x)
         return x
 
