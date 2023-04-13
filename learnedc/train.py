848912ecf8b433acf3ff1d44398847338dedce1e
Angold-4 <wangold4w@gmail.com>
2023-04-13 11:01:50 +0800

    perform random_hyperparameter_search on current model

--- learnedc/train.py
+++ learnedc/train.py
@@ -1,6 +1,7 @@
 from network import VAE
 import torch
 import os
+import random
 import torchvision.transforms as transforms
 from torchvision.datasets import STL10
 from torch.utils.data import DataLoader
@@ -10,6 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Load the VAE model
+num_epochs = 50
 latent_dim = 256
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 vae = VAE(latent_dim).to(device)
@@ -40,12 +42,78 @@ optimizer = optim.Adam(vae.parameters(), lr=1e-4, weight_decay=1e-5)
 
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, verbose=True)
 
+def train_vae(vae, optimizer, scheduler, num_epochs):
+    test_loss = 0 
+    for epoch in range(num_epochs):
+        vae.train()
+        train_loss = 0
+        for _, (data, _) in enumerate(trainloader):
+            data = data.to(device)
+            optimizer.zero_grad()
+            recon_batch, mu_main, logvar_main, mu_hyper, logvar_hyper = vae(data)
+            loss = loss_function(recon_batch, data, mu_main, logvar_main, mu_hyper, logvar_hyper)
+            loss.backward()
+            train_loss += loss.item()
+            optimizer.step()
+
+        print(f'====> Epoch: {epoch} Average loss: {train_loss / len(trainloader.dataset):.4f}') # type: ignore
+
+        # Evaluation loop
+        vae.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for _, (data, _) in enumerate(testloader):
+                data = data.to(device)
+                recon_batch, mu_main, logvar_main, mu_hyper, logvar_hyper = vae(data)
+                test_loss += loss_function(recon_batch, data, mu_main, logvar_main, mu_hyper, logvar_hyper).item()
+
+        test_loss /= len(testloader.dataset) # type: ignore
+        print(f'====> Test set loss: {test_loss:.4f}')
+        scheduler.step(test_loss)
+
+    # Return the test loss for the last epoch
+    return test_loss
+
+def random_hyperparameter_search(num_searches):
+    latent_dims = [64, 128, 256, 512]
+    learning_rates = [1e-4, 5e-4, 1e-3, 5e-3]
+
+    best_loss = float('inf')
+    best_hyperparams = None
+
+    for _ in range(num_searches):
+        # Sample hyperparameters
+        latent_dim = random.choice(latent_dims)
+        learning_rate = random.choice(learning_rates)
+        print(f'Hyperparameters: latent_dim={latent_dim}, learning_rate={learning_rate}')
+
+        # Initialize the model, optimizer and scheduler
+        vae = VAE(latent_dim).to(device)
+        optimizer = optim.Adam(vae.parameters(), lr=learning_rate, weight_decay=1e-5)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, verbose=True)
+
+        # Train the model and get the test loss
+        test_loss = train_vae(vae, optimizer, scheduler, num_epochs)
+
+        # Update the best loss and hyperparameters
+        if test_loss < best_loss:
+            best_loss = test_loss
+            best_hyperparams = {'latent_dim': latent_dim, 'learning_rate': learning_rate}
+            print(f'New best hyperparameters: {best_hyperparams}, test loss: {test_loss:.4f}')
+
+    return best_hyperparams
+
 if __name__ == '__main__':
     if not os.path.exists("model"):
         os.makedirs("model")
     if not os.path.exists("vis"):
         os.makedirs("vis")
 
+    num_searches = 10   
+    best_hyperparams = random_hyperparameter_search(num_searches)
+    print(f'Best hyperparameters: {best_hyperparams}')
+
+"""
     # Training loop
     num_epochs = 200
     for epoch in range(num_epochs):
@@ -111,3 +179,5 @@ if __name__ == '__main__':
             # Save the visualization
             plt.savefig(f"vis/visualization_{epoch}.png")
             plt.close(fig)
+
+  """
